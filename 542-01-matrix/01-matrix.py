693c1208cc6d91f4c2c5ef3ab3372bf8d8364f88
anandu1205 <anandu1205@gmail.com>
2026-05-27 22:54:37 +0530

    Time: 137 ms (37.98%) | Memory: 23.1 MB (19.03%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        ans = [[0] * n for _ in range(m)]
+        queue=deque()
+        visited=set()
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==0:
+                    queue.append((i,j))
+                    visited.add((i,j))
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+        
+        while queue:
+            r,c=queue.popleft()
+            
+            for dr,dc in directions:
+                nr,nc=r+dr,c+dc
+                if nr>=0 and nc>=0 and nr<m and nc<n and (nr,nc) not in visited :
+                    ans[nr][nc] = ans[r][c] + 1
+                    visited.add((nr,nc))
+                    queue.append((nr,nc))
+        return ans
+
+        
