29e8898a0e922069053a7439a01e6e75b2ac0ea3
anandu1205 <anandu1205@gmail.com>
2026-05-29 19:07:05 +0530

    Time: 27 ms (51.39%) | Memory: 20 MB (45.32%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,27 +1,25 @@
-from typing import List
-import heapq
-
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        pq = [(grid[0][0], 0, 0)]  # (time, row, col)
-        visited = set()
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        
+        n=len(grid)
+        import heapq
+        pq=[(grid[0][0],0,0)] # max_time,r,c
+        max_time=-1
+        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+        visited=set()
         while pq:
-            time, r, c = heapq.heappop(pq)
-            
+            time,r,c=heapq.heappop(pq)
             if (r, c) in visited:
-                continue
-            visited.add((r, c))
-            
-            # Reached destination
-            if r == n - 1 and c == n - 1:
+               continue
+            visited.add((r,c))
+            if r==n-1 and c==n-1:
                 return time
-            
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                
-                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in visited:
+            for dr,dc in directions:
+                nr,nc=r+dr,c+dc
+                if nr>=0 and nc>=0 and nr<n and nc<n and (nr,nc) not in visited:
+
                     new_time = max(time, grid[nr][nc])
                     heapq.heappush(pq, (new_time, nr, nc))
+                    
+                    
+        return time
+        
