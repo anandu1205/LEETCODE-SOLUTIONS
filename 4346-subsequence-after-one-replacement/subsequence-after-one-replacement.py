bc2559f1411c33c2044fc95388a5f687fb4373ef
anandu1205 <anandu1205@gmail.com>
2026-07-11 11:51:50 +0530

    Time: 153 ms (91.48%) | Memory: 20.1 MB (80.88%) - LeetSync

---
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canMakeSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        m = len(s)
+
+        for c in t:
+            if s[i] == c:
+                i += 1
+
+            i = max(i, j + 1)
+
+            if s[j] == c:
+                j += 1
+
+            if i == m or j == m:
+                return True
+
+        return False
