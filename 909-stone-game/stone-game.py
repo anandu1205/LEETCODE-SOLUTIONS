3b905148390d208e5770a9e4ef2921d31aa5d74e
anandu1205 <anandu1205@gmail.com>
2026-08-25 21:27:02 +0530

    Time: 2 ms (36.47%) | Memory: 19.3 MB (68.14%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,6 +1,15 @@
-from typing import List
-
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        Alice=0
+        Bob=0
+        n=len(piles)
+        while not(n<=0):
+            maximum=max(piles[0],piles[-1])
+            minimum=min(piles[0],piles[-1])
+            Alice+=maximum
+            Bob+=minimum
+            piles.remove(maximum)
+            piles.remove(minimum)
+            n-=2
+        return Alice>Bob
         
