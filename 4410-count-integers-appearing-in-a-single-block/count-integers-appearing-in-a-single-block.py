399e60a710c1ebeb7d2df2548b9cd35ca1dbbd3a
anandu1205 <anandu1205@gmail.com>
2026-09-10 17:33:49 +0530

    Time: 4 ms (8.19%) | Memory: 19.3 MB (34.08%) - LeetSync

--- 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.py
+++ 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.py
@@ -1,20 +1,25 @@
 class Solution:
-    def countSpecialIntegers(self, nums: List[int]) -> int:
-        first = {}
-        last = {}
-        freq = {}
-
-        for i, x in enumerate(nums):
-            if x not in first:
-                first[x] = i
-
-            last[x] = i
-            freq[x] = freq.get(x, 0) + 1
-
-        ans = 0
-
-        for x in freq:
-            if last[x] - first[x] + 1 == freq[x]:
-                ans += 1
-
-        return ans
+    def countSpecialIntegers(self, nums: list[int]) -> int:
+        special=0
+        maximum={}
+        minimum={}
+        for i in range(len(nums)):
+            value=nums[i]
+            minimum[value]=nums.index(value)
+        for i in range(len(nums)):
+            value=nums[i]
+            value1=minimum[value]
+            arr=nums[i:]
+            for j in range(len(arr)):
+                if (j+i)>value1 and arr[j]==value:
+                    value1=j+i
+            maximum[value]=value1
+        visited=set()
+        for i in range(len(nums)):
+            if nums[i] in visited:
+                continue
+            visited.add(nums[i])
+            value=nums[i]
+            if (maximum[value]-minimum[value]+1)==nums.count(value):
+                special+=1
+        return special
